b20058737a1a848ca9e09fe7785819d85dc62b90
Noah Phillips <noahphillips@Noahs-MacBook-Pro.local>
2018-06-06 18:31:37 -0400

    server will watch the /webhooks router

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express'),
     app = express(),
     router = express.Router(),
     bodyParser = require('body-parser'),
-    cors = require('cors'),
     port = process.env.PORT || 3000,
     git = require('simple-git')
+
+app.use(bodyParser.urlencoded({
+    limit: '500mb',
+    extended: true,
+    type: 'application/x-www-form-urlencoded'
+}))
+
+app.use(bodyParser.json({
+    limit: '500mb',
+    type: 'application/*'
+}))
+
+router.get('/webooks', (req, res) => {
+    
+})
+
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Server active at http://localhost:${port}`)
